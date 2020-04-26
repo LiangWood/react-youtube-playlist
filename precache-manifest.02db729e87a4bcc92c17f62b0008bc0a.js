24542c59d3df616bc9f84b3130c7e93341143f55
Jack Liang <zily68074@gmail.com>
2020-04-26 21:05:24 +0800

    Updates

---
+++ precache-manifest.02db729e87a4bcc92c17f62b0008bc0a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "825b7786ce5ddbdb7643f924eaf33c04",
+    "url": "/react-youtube-playlist/index.html"
+  },
+  {
+    "revision": "9c70ed8307d8931e4b06",
+    "url": "/react-youtube-playlist/static/css/main.cc468cdd.chunk.css"
+  },
+  {
+    "revision": "616d0a63991672b18fcb",
+    "url": "/react-youtube-playlist/static/js/2.4c6c8d2e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-youtube-playlist/static/js/2.4c6c8d2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c70ed8307d8931e4b06",
+    "url": "/react-youtube-playlist/static/js/main.79edf668.chunk.js"
+  },
+  {
+    "revision": "a7045177eaed3fdff055",
+    "url": "/react-youtube-playlist/static/js/runtime-main.b953e9ca.js"
+  }
+]);
